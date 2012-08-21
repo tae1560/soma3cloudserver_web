cca63d4e947ff64456c2bdb8a9de8691d00ff5a3
LEETAEHO <tae1560@gmail.com>
2012-08-20 22:31:04 -0700

    Added session validation function

--- util.php
+++ util.php
@@ -16,10 +16,34 @@ function messageWithAlert($message) {
 	echo "</script>";
 }
 
-function redirectToURL($url, $delay=0) {
+function redirectToURL($url, $delay = 0) {
 	echo "<script type=\"text/javascript\" charset=\"utf-8\">";
 	echo "window.location = \"$url\";";
 	echo "</script>";
 }
-    
+
+function validate_session() {
+	session_start();
+	
+	$id = $_SESSION['id'];
+	$token = $_SESSION['token'];
+	if ($id == null || $token == null) {
+		return false;
+	}
+
+	$args = array('id' => $id, 'token' => $token);
+	$response = getResponstWithPost("/api/user/get_information/index.php?response_object=json", $args);
+	$data = json_decode($response);
+
+	// case별로 처리하기
+	switch ($data->result_code) {
+		case 401 :
+		// session timeout
+			$id = $_SESSION['id'] = 0;
+			$token = $_SESSION['token'] = 0;
+			return false;
+	}
+
+	return true;
+}
 ?>
