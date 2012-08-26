cbf0559179ce0da2a8e3091f70c4df5699062eb6
LEETAEHO <tae1560@gmail.com>
2012-08-26 10:33:42 +0900

    Started make login page

--- index.php
+++ index.php
@@ -1,5 +1,51 @@
 <?PHP
 
-// check if login success
+// return : true if login status
+function isLogin() {
+	if ($_SESSION['token'] == null) {
+		return false;
+	} 
+	
+	
+}
 
+// args : reference of variable for return with json
+function process() {
+	global $configure;
+
+	// invalid argument block
+	if (validArguments($id) == false) {
+		$returnValue['result'] = $configure['results']['invalid_argument']['message'];
+		$returnValue['result_code'] = $configure['results']['invalid_argument']['code'];
+		return;
+	}
+	
+	// token validation block
+	if (validToken($token) == false) {
+		$returnValue['result'] = $configure['results']['failed_authentication']['message'];
+		$returnValue['result_code'] = $configure['results']['failed_authentication']['code'];
+		return;
+	}
+	
+	// remove session
+	$result = removeUser($id);
+	
+	if ($result == true) {
+		$returnValue['result'] = $configure['results']['success']['message'];
+		$returnValue['result_code'] = $configure['results']['success']['code'];
+	} else {
+		$returnValue['result'] = $configure['results']['internal_error']['message'];
+		$returnValue['result_code'] = $configure['results']['internal_error']['code'];
+	}
+}
+
+function main() {
+	//process($returnValue);
+
+	// output return value
+	//output_result($returnValue);
+}
+
+// start main
+main();
 ?>
